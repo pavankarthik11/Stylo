f7e5de3a82129c6b77debf8e93f30e0bda1ace03
pavankarthik11 <pavankarthiktellakula11@gmail.com>
2026-07-09 21:56:50 +0530

    feat: implement user registration route and controller, update app structure with route imports

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,10 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+
+const registerUser = asyncHandler(async(requestAnimationFrame,res)=>{
+    res.status(200).json({
+        message: "Stylo"
+    })
+})
+
+export {registerUser}
